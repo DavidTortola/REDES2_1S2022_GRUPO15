2b3e654cd746ad7994b77fb942c038d055733bd0
David Tórtola <davidtortola_@hotmail.com>
2022-11-05 01:06:20 -0600

    commit

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,27 +1,16 @@
 function obtenerDatos() {
     console.log("llamando al login")
-    // $.ajax({
-    //     url: "http://10.0.26.48/user/codigoUsuario",
-    //     type: "post",
-    //     contentType: "application/json; charset=utf-8",
-    //     success: function (data, textStatus, jQxhr) {
-    //         var result = JSON.parse(data);
-    //         console.log(result);
-    //         console.log('hola erix y ambrose');
-    //     },
-    //     error: function (jqXhr, textStatus, errorThrown) {
-    //         console.log(errorThrown);
-    //     },
-    // });
-
-
-    let data = { element: "barium" };
-    fetch("http://10.0.133.67/user/codigoUsuario", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-        timeout: 5000
-    }).then(res => {
-        console.log("Request complete! response:", res);
+    $.ajax({
+        url: "http://10.0.133.67/user/codigoUsuario",
+        type: "post",
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, jQxhr) {
+            var result = JSON.parse(data);
+            console.log(result);
+            console.log('hola erix y ambrose');
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
     });
 }
