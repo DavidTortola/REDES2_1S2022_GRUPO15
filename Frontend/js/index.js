066ce7f4bfb56e96c4a2211e07196e4da252be7e
David Tórtola <davidtortola_@hotmail.com>
2022-11-04 23:41:10 -0600

    update

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -3,8 +3,6 @@ function obtenerDatos() {
     $.ajax({
         url: "http://10.0.26.48:80/user/codigoUsuario",
         type: "post",
-        dataType: "text",
-        data: JSON.stringify(""),
         contentType: "application/json; charset=utf-8",
         success: function (data, textStatus, jQxhr) {
             var result = JSON.parse(data);
