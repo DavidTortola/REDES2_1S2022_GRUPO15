a2cf2c33923545890fd93bb13899c7b0755c2b72
David Tórtola <davidtortola_@hotmail.com>
2022-11-04 23:28:41 -0600

    cambio

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,17 +1,18 @@
 function obtenerDatos() {
     console.log("llamando al login")
     $.ajax({
-      url: "http://10.0.26.48:80/user/codigoUsuario",
-      dataType: "text",
-      type: "post",
-      contentType: "application/json; charset=utf-8",
-      success: function (data, textStatus, jQxhr) {
-        var result = JSON.parse(data);
-        console.log(result);
-        console.log('hola erix y ambrose');
-      },
-      error: function (jqXhr, textStatus, errorThrown) {
-        console.log(errorThrown);
-      },
+        url: "http://10.0.26.48:80/user/codigoUsuario",
+        type: "post",
+        dataType: "text",
+        data: JSON.stringify(""),
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, jQxhr) {
+            var result = JSON.parse(data);
+            console.log(result);
+            console.log('hola erix y ambrose');
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
     });
-  }
+}
