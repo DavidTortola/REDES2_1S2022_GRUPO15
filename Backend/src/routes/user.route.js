7e803c921c8460b9b6607e20cfc0e678311cb982
David Tórtola <davidtortola_@hotmail.com>
2022-11-04 23:37:25 -0600

    cambio

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers/user.controller");
 
+router.use(cors({
+    origin: '*'
+}));
+
+
 router.post("/codigoUsuario", ctrl.codigoUsuario);
 
 module.exports = router;
