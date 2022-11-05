3ee180c0261d9a58a8c3bde863e11791e7974ca0
David Tórtola <davidtortola_@hotmail.com>
2022-11-04 23:47:25 -0600

    cambio

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,18 +3,9 @@ var cors = require('cors');
 const app = express();
 const port = 80;
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
-app.options('/user', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
-  });
+
 
 //routes
 const userRoute = require('./routes/user.route');
