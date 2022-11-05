97dfd8934d71e29e446a407c8d230cdfed505561
David Tórtola <davidtortola_@hotmail.com>
2022-11-05 00:40:02 -0600

    cambio

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,16 +1,26 @@
 function obtenerDatos() {
     console.log("llamando al login")
-    $.ajax({
-        url: "http://10.0.26.48/user/codigoUsuario",
-        type: "post",
-        contentType: "application/json; charset=utf-8",
-        success: function (data, textStatus, jQxhr) {
-            var result = JSON.parse(data);
-            console.log(result);
-            console.log('hola erix y ambrose');
-        },
-        error: function (jqXhr, textStatus, errorThrown) {
-            console.log(errorThrown);
-        },
+    // $.ajax({
+    //     url: "http://10.0.26.48/user/codigoUsuario",
+    //     type: "post",
+    //     contentType: "application/json; charset=utf-8",
+    //     success: function (data, textStatus, jQxhr) {
+    //         var result = JSON.parse(data);
+    //         console.log(result);
+    //         console.log('hola erix y ambrose');
+    //     },
+    //     error: function (jqXhr, textStatus, errorThrown) {
+    //         console.log(errorThrown);
+    //     },
+    // });
+
+
+    let data = { element: "barium" };
+    fetch("http://10.0.26.48/user/codigoUsuario", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(res => {
+        console.log("Request complete! response:", res);
     });
 }
