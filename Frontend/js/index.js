ece8e18be1c0c49786fbd72bedc9ba852c917960
David Tórtola <davidtortola_@hotmail.com>
2022-11-05 08:52:28 -0600

    cambio

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,7 +1,7 @@
 function obtenerDatos() {
     console.log("llamando al login")
     $.ajax({
-        url: "http://44.197.229.70:3000/api/datos",
+        url: "http://44.197.229.70:3000/api/home",
         type: "get",
         contentType: "application/json; charset=utf-8",
         success: function (data, textStatus, jQxhr) {
